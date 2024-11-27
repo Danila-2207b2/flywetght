3072d1e4a82383606a93c124aa337518be5bc1c1
Danila-2207b2 <cheesekiss007@gmail.com>
2024-11-27 22:29:00 +0700

    Add files via upload

---
+++ Flyweight_pattern/CharacterFactory.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Flyweight_pattern
+{
+    class CharacterFactory
+    {
+        private Dictionary<string, Character> characters = new Dictionary<string, Character>(); 
+
+        public Character GetCharacter(string name, string type, string image)
+        {
+            string key = $"{name}-{type}"; 
+
+            if (!characters.ContainsKey(key))
+            {
+                Character newCharacter = new Character(name, type, image);
+                characters.Add(key, newCharacter);
+                Console.WriteLine($"Создан новый персонаж: {newCharacter}");
+                return newCharacter;
+            }
+            else
+            {
+                Console.WriteLine($"существующий персонаж: {characters[key]}");
+                return characters[key];
+            }
+        }
+    }
+}
---
+++ Flyweight_pattern/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Flyweight_pattern
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CharacterFactory factory = new CharacterFactory();
+
+            Character warrior1 = factory.GetCharacter("Obla", "Warrior", "warrior.png");
+            Character warrior2 = factory.GetCharacter("Sanya", "Warrior", "warrior.png"); 
+            Character mage = factory.GetCharacter("Danila", "Mage", "mage.png");
+            Character warrior3 = factory.GetCharacter("Wan", "Warrior", "warrior.png");
+
+
+            warrior1.Level = 5;
+            warrior1.Experience = 1000;
+
+            Console.WriteLine($"\nInformation about characters:");
+            Console.WriteLine(warrior1);
+            Console.WriteLine(warrior2); 
+            Console.WriteLine(mage);
+            Console.WriteLine(warrior3);
+
+            Console.Read();
+        }
+    }
+}
